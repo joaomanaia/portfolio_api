c3fd720df8ef72d2a577e596c172dcc5263a96e5
joaomanaia <joaopfmanaia@gmail.com>
2023-12-27 14:30:49 +0000

    Refactor CSS and add ProjectAuthors component

--- app/(home)/components/projects/project-timeline-item.tsx
+++ app/(home)/components/projects/project-timeline-item.tsx
@@ -4,6 +4,7 @@ import PostTimeLineDataType from "@/data/timeline/PostTimeLineDataType"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
+import { ProjectAuthors } from "./project-authors"
 
 interface ProjectTimelineItemProps {
   project: PostTimeLineDataType
@@ -12,14 +13,7 @@ interface ProjectTimelineItemProps {
 export const ProjectTimelineItem: React.FC<ProjectTimelineItemProps> = ({ project }) => {
   return (
     <li className="flex flex-col items-start justify-center w-full h-auto">
-      <div className="flex">
-        <AuthorsImages authors={project.authors} />
-
-        <div className="ml-4">
-          <p className="text-foreground/50">{project.postDate}</p>
-          <AuthorsNames authors={project.authors} />
-        </div>
-      </div>
+      <ProjectAuthors authors={project.authors} postDate={project.postDate} />
 
       <div className="grow flex ml-5 mt-1 w-full">
         <div className={`w-1 h-auto bg-primary mb-4 rounded-2xl`}></div>
@@ -44,66 +38,11 @@ export const ProjectTimelineItem: React.FC<ProjectTimelineItemProps> = ({ projec
           </div>
           <p className="text-xl mt-4">{project.postTitle}</p>
           <p className="mt-2">{project.postDescription}</p>
-          <Button className="mt-4">Ver mais informações</Button>
+          <Button className="mt-4" asChild>
+            <Link href={`/projetos/${project.route}`}>Ver mais informações</Link>
+          </Button>
         </div>
       </div>
     </li>
-  )
-}
-
-interface AuthorsNamesProps {
-  authors: UserType[]
-}
-
-const AuthorsNames: React.FC<AuthorsNamesProps> = ({ authors }) => {
-  return (
-    <p className="text-foreground text-lg">
-      {authors.map((author, index) => (
-        <React.Fragment key={author.linkName}>
-          <Link href={author.linkName}>{author.name}</Link>
-          {index < authors.length - 1 && ", "}
-        </React.Fragment>
-      ))}
-    </p>
-  )
-}
-
-interface AuthorsImagesProps {
-  authors: UserType[]
-}
-
-const AuthorsImages: React.FC<AuthorsImagesProps> = ({ authors }) => {
-  if (authors.length == 1) {
-    return (
-      <Link href={authors[0].linkName} className="relative w-11 h-11">
-        <Image
-          className="rounded-full"
-          src={authors[0].photoUrl}
-          alt={authors[0].name}
-          layout="fill"
-        />
-      </Link>
-    )
-  }
-
-  return (
-    <div className="w-11 h-11 -space-y-4">
-      {authors.map((author) => (
-        <div
-          key={author.linkName}
-          className="relative w-11 h-11 border-2 border-primary rounded-full"
-        >
-          <Link href={author.linkName}>
-            <Image
-              className="rounded-full"
-              src={author.photoUrl}
-              alt={author.name}
-              layout="fill"
-              objectFit="cover"
-            />
-          </Link>
-        </div>
-      ))}
-    </div>
   )
 }
