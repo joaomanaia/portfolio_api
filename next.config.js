8d94a9fee75fa7f7d82820c6768c2922fd7b7d97
joaomanaia <joaopfmanaia@gmail.com>
2023-12-27 17:56:53 +0000

    Add X-Robots-Tag header for production environment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com", "cdn.discordapp.com"],
   },
+  async headers() {
+    if (process.env.NODE_ENV !== "production") return []
+
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
